bfd0649dbd6cfc717768716589f23340cf9f4e65
Administrator <liuqianglan@sina.com>
2020-05-29 11:32:00 +0800

    redis

--- redistest.php
+++ redistest.php
@@ -1,22 +1,26 @@
 <?php
 
-$res = addGoods();
+$redis = new Redis();
+$redis->connect('127.0.0.1', '6379');
+$listKey = '20200529_goods_list';
+$redis->lTrim($listKey, 1, 30);
+
+$res = getGoodsList($redis);
 var_dump($res);
 /*
  * 添加商品
  */
-function addGoods()
+function addGoods($redis)
 {
     $count = 30;
-    $listKey = '20200529_goods_list';
-
-    $redis = new Redis();
-    $redis->connect('127.0.0.1', '6379');
 
     for ($i = 0; $i < $count; $i++) {
         $redis->rPush($listKey, $i);
     }
+}
 
+function getGoodsList($redis)
+{
     $goods = $redis->lRange($listKey, 0, -1);
     return $goods;
 }
