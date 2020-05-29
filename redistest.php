00706950c11a221ef6b9ea23f7c4170b7e81ce6d
Administrator <liuqianglan@sina.com>
2020-05-29 16:11:53 +0800

    redis

--- redistest.php
+++ redistest.php
@@ -3,10 +3,14 @@
 $redis = new Redis();
 $redis->connect('127.0.0.1', '6379');
 $listKey = '20200529_goods_list';
-$redis->lTrim($listKey, 1, 30);
+//$redis->lTrim($listKey, 1, 30);
 
-$res = getGoodsList($redis, $listKey);
-var_dump($res);
+
+//$res = getGoodsList($redis, $listKey);
+//var_dump($res);
+
+$row = kill($redis, $listKey);
+echo $row;
 /*
  * 添加商品
  */
@@ -25,5 +29,20 @@ function getGoodsList($redis, $listKey)
     return $goods;
 }
 
+function kill($redis, $listKey)
+{
+    //用户id
+    $uuid = md5(uniqid('user') . time());
+    $orderKey = '20200529_buy_order';
+    $failuser = '20200529_fail_user';
+    $goodsId = $redis->lPop($listKey);
+    if ($goodsId) {
+        $redis->hSet($orderKey, $goodsId, $uuid);
+    } else {
+        $redis->incr($failuser);
+    }
+
+    return 'SUCCESS';
+}
 
 
