63d819d5d49b1f290d30934fdf6fc53fa960266b
Administrator <liuqianglan@sina.com>
2020-05-29 16:55:57 +0800

    redis

--- redistest.php
+++ redistest.php
@@ -9,11 +9,11 @@ $listKey = '20200529_goods_list';
 //$res = getGoodsList($redis, $listKey);
 //var_dump($res);
 
-//kill($redis, $listKey);
+kill($redis, $listKey);
 //echo $row;
 
-$row = $redis->hGetAll('20200529_buy_order');
-print_r($row);
+//$row = $redis->hGetAll('20200529_buy_order');
+//print_r($row);
 /*
  * 添加商品
  */
