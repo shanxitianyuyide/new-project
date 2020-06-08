288bc7979681bd87345af3bb8b1e0d3958d18260
Administrator <liuqianglan@sina.com>
2020-06-08 14:51:55 +0800

    redis

--- server.php
+++ server.php
@@ -1,20 +1,10 @@
 <?php
-//创建Server对象，监听 127.0.0.1:9501端口
-    $serv = new Swoole\Server('127.0.0.1', 9501);
+$http = new Swoole\Http\Server("0.0.0.0", 9501);
 
-//监听连接进入事件
-    $serv->on('connect', function ($serv, $fd) {
-        echo 'Client: Connect.\n';
-    });
-//监听数据接收事件
-$serv->on('Receive', function ($serv, $fd, $from_id, $data) {
-    $serv->send($fd, "Server: ".$data);
+$http->on('request', function ($request, $response) {
+    var_dump($request->get, $request->post);
+    $response->header("Content-Type", "text/html; charset=utf-8");
+    $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
 });
 
-//监听连接关闭事件
-$serv->on('Close', function ($serv, $fd) {
-    echo "Client: Close.\n";
-});
-
-//启动服务器
-$serv->start();
+$http->start();
