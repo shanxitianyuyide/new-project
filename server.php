ff0ff84d2488feebf097cfb889077cf0684dee79
Administrator <liuqianglan@sina.com>
2020-06-10 14:53:22 +0800

    redis

--- server.php
+++ server.php
@@ -1,5 +1,5 @@
 <?php
-$http = new Swoole\Http\Server("0.0.0.0", 9501);
+$http = new swoole_http_server("0.0.0.0", 9501);
 
 $http->on('request', function ($request, $response) {
     var_dump($request->get, $request->post);
