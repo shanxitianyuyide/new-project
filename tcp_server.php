ed3944a33c273aa006f063b3cf082fcc72e7822d
Administrator <liuqianglan@sina.com>
2020-06-11 10:03:32 +0800

    redis

--- tcp_server.php
+++ tcp_server.php
@@ -19,8 +19,23 @@ $serv = swoole_server($host, $port, $mode, $socke_type);
  * close： 关闭链接
  */
 $event = 'connect';
+//$serv 服务端信息  $fd 客户端信息
 $serv->on($event, function ($serv, $fd){
     var_dump($serv);
     var_dump($fd);
-    echo '建立链接';
+    echo '建立链接\n';
 });
+
+//$reactor_id 客户端ID   $data 接收数据
+$serv->on('receive', function ($serv, $fd, $reactor_id, $data) {
+    echo '接受数据\n';
+    var_dump($reactor_id);
+    var_dump($data);
+});
+
+$serv->on('close', function ($serv, $fd) {
+    echo '断开连接';
+});
+
+$serv->start();
+
