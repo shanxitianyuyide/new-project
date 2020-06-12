dd36659545845221c83961424ba37915aeb92c6d
Administrator <liuqianglan@sina.com>
2020-06-12 17:23:29 +0800

    redis

---
+++ timing.php
@@ -0,0 +1,10 @@
+<?php
+//tick 定时器会持续触发
+Swoole\Timer::tick(1000, function ($timer_id) {
+    echo "定时器-{$timer_id}\n";
+});
+
+//after 定时器在指定的时间后执行，一次性定时器，执行完成后就会销毁
+Swoole\Timer::after(5000, function ($timer_id) {
+    echo "定时器-{$timer_id}\n";
+});
