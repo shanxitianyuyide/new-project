68d7215c6d39b0bada4b2cfe8ca8f01f0729aa6b
Administrator <liuqianglan@sina.com>
2020-06-12 17:27:48 +0800

    redis

--- timing.php
+++ timing.php
@@ -5,6 +5,7 @@ Swoole\Timer::tick(1000, function ($timer_id) {
 });
 
 //after 定时器在指定的时间后执行，一次性定时器，执行完成后就会销毁
-Swoole\Timer::after(5000, function ($timer_id) {
-    echo "定时器-{$timer_id}\n";
+$str = 'swoole';
+Swoole\Timer::after(5000, function () use ($str) {
+    echo "定时器-{$str}\n";
 });
