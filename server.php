4189a0e58811c3ccbab8a87fb5c1a0d2507726da
Administrator <liuqianglan@sina.com>
2020-06-12 16:00:32 +0800

    redis

--- server.php
+++ server.php
@@ -2,8 +2,13 @@
 $http = new swoole_http_server("0.0.0.0", 9501);
 
 $http->on('request', function ($request, $response) {
+    if ($request->server['path_info'] == '/favicon.ico' || $request->server['request_uri'] == '/favicon.ico') {
+        $response->end();
+        return;
+    }
     var_dump($request->get, $request->post);
     $response->header("Content-Type", "text/html; charset=utf-8");
+    //输出
     $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
 });
 
