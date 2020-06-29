1fdb3e540239b603e3eb98d38180255e89dfc264
Administrator <liuqianglan@sina.com>
2020-06-29 10:43:32 +0800

    redis

--- server.php
+++ server.php
@@ -2,7 +2,7 @@
 $http = new swoole_http_server("0.0.0.0", 9501);
 
 $http->on('request', function ($request, $response) {
-    var_dump($response);
+    echo $request->header['host'];
     $response->header("Content-Type", "text/html; charset=utf-8");
     //输出
     $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
