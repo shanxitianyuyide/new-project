ed52e9a7123a13cf3364771a48c2b20cefd47a21
Administrator <liuqianglan@sina.com>
2020-07-07 15:16:19 +0800

    redis

--- ws_server.php
+++ ws_server.php
@@ -1,23 +1,32 @@
 <?php
-//创建websocket服务器对象，
-$ws = new Swoole\WebSocket\Server('0.0.0.0', 9502);
+class SwooleWebsocket
+{
+    public $websocket;
 
-//监听websocket链接打开事件
-$ws->on('open', function ($ws, $request) {
-    var_dump($request->fd, $request->get, $request->server);
-    //push发送消息
-    $ws->push($request->fd, "hello,welcome\n");
-});
+    public function __construct()
+    {
+        $this->websocket = new Swoole\WebSocket\Server("0.0.0.0", 9501);
+        //监听客户端链接
+        $this->websocket->on('open', function (Swoole\WebSocket\Server $server, $request) {
+            $fd[] = $request->fd;
+            $GLOBALS['fd'][] = $fd;
+        });
+        //监听websocket消息事件
+        $this->websocket->on('message', function (Swoole\WebSocket\Server $server, $frame) {
+            $msg =  'from'.$frame->fd.":{$frame->data}\n";
+            foreach($GLOBALS['fd'] as $aa){
+                foreach($aa as $i){
+                    $this->websocket->push($i,$msg);
+                }
+            }
+        });
 
-//监听websocket消息事件
-$ws->on('message', function ($ws, $frame) {
-    echo "Message: {$frame->data}\n";
-    $ws->push($frame->fd, "server: {$frame->data}");
-});
+        $this->websocket->on('close', function ($ser, $fd) {
+            echo "client-{$fd} is closed\n";
+        });
 
-//监听websocket关闭事件
-$ws->on('close', function ($ws, $fd) {
-    echo "clien-{$fd} is closed\n";
-});
+        $this->websocket->start();
+    }
+}
 
-$ws->start();
+new SwooleWebsocket();
